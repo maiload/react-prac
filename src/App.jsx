47d892c78b63fe1318dfe85e355a900e2b228ba4
JJ Lee <maiload77@gmail.com>
2024-07-08 22:48:05 +0900

    add: 미니 블로그 실습 프로젝트 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import styled from "styled-components";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import MainPage from "./component/page/MainPage.jsx";
+import PostWritePage from "./component/page/PostWirtePage.jsx";
+import PostViewPage from "./component/page/PostViewPage.jsx";
+
+const MainTitleText = styled.p`
+    font-size: 24px;
+    font-weight: bold;
+    text-align: center;
+`;
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <BrowserRouter>
+          <MainTitleText>정진의 미니 블로그</MainTitleText>
+          <Routes>
+              <Route index element={<MainPage />} />
+              <Route path="post-write" element={<PostWritePage />} />
+              <Route path="post/:postId" element={<PostViewPage />} />
+          </Routes>
+      </BrowserRouter>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 
-import Blocks from "./chapter_15/Blocks.jsx";
+import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Blocks />
+    <App />
   </React.StrictMode>,
 )
